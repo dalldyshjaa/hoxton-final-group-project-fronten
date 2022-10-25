d8297f21849d3d3344821115c42780af67dd52c5
taulant bytyqi <taulantbytyqi223@gmail.com>
2022-10-25 22:17:16 +0200

    asd

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,3 @@
+export function ProfilePage({ userOn }: any) {
+  return <h1>asda</h1>;
+}
--- src/pages/SignedIn.tsx
+++ src/pages/SignedIn.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { ProfilePage } from "./ProfilePage";
+
 export function SignedIn({ userOn, setUserOn }: any) {
-  return <div>asd</div>;
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/get-all-rooms")
+      .then((resp) => resp.json())
+      .then((rooms) => setRooms(rooms));
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<ProfilePage userOn={userOn} />} />
+    </Routes>
+  );
 }
