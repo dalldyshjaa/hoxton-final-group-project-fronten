82926df54047a2f070fd194c313b1e7e65a1ba31
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-10-28 00:59:25 +0200

    /////

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Footer } from "../components/Footer";
 import { SaveModal } from "../components/SaveModal";
 import { Menu, Tick, Verified } from "../Icons";
 
@@ -159,6 +160,7 @@ export function ProfilePage({ userOn, SignOut }: any) {
             </aside>
             <section></section>
           </div>
+          <Footer />
         </>
       ) : null}
     </>
