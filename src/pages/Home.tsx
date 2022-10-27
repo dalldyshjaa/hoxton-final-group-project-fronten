aed016763a636cf01640c5d8354daea2c944fad8
Aris Alshiqi <arisalshiqi111@gmail.com>
2022-10-27 14:52:24 +0200

    ////

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,97 +34,6 @@ export function Home({ setUserOn }: any) {
     
 
 
-    const roomss = [
-        {
-            id: 1,
-            title: "Casa de campo",
-            city: "Cali",
-            country: "Colombia",
-            reviewsNr: 5,
-            review: 4.5,
-            price: "100.000",
-            guestsLimit: 4,
-            description: "Casa de campo con piscina y jacuzzi",
-            rules: "No se permite fumar",
-            images: [
-                {
-                    id: 1,
-                    image: "https://img.freepik.com/free-photo/yellow-sofa-wooden-table-living-room-interior-with-plant_41470-3559.jpg?w=2000",
-                    roomId: 1
-                },
-                {
-                    id: 2,
-                    image: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?cs=srgb&dl=pexels-vecislavas-popa-1571460.jpg&fm=jpg",
-                    roomId: 1
-                },
-                {
-                    id: 3,
-                    image: "https://media.gettyimages.com/photos/bohemian-living-room-interior-3d-render-picture-id1182454657?s=612x612",
-                    roomId: 1
-                }
-            ]
-        },
-        {
-            id: 2,
-            title: "Casa de campo",
-            city: "Cali",
-            country: "Colombia",
-            reviewsNr: 5,
-            review: 4.5,
-            price: "100.000",
-            guestsLimit: 4,
-            description: "Casa de campo con piscina y jacuzzi",
-            rules: "No se permite fumar",
-            images: [
-                {
-                    id: 1,
-                    image: "https://media.istockphoto.com/photos/classic-gray-interior-with-armchairs-sofa-coffee-table-lamps-flowers-picture-id1210123439?b=1&k=20&m=1210123439&s=612x612&w=0&h=DxEqjv85A2bn-uJCodJG22C1L4Q2MuzFzhj4it_gVDg=",
-                    roomId: 2
-                },
-                {
-                    id: 2,
-                    image: "https://media.istockphoto.com/photos/scandinavian-concept-of-living-room-interior-with-design-sofa-coffee-picture-id1251694108?k=20&m=1251694108&s=612x612&w=0&h=8zH7jqtg_QgLMJMxUq3uBpA7l47LUht32_xT4RHn1EI=",
-                    roomId: 2
-                },
-                {
-                    id: 3,
-                    image: "https://media.gettyimages.com/photos/modern-living-room-interior-3d-render-picture-id1293762741?s=612x612",
-                    roomId: 2
-                }
-            ]
-        },
-        {
-            id: 3,
-            title: "Casa de campo",
-            city: "Cali",
-            country: "Colombia",
-            reviewsNr: 5,
-            review: 4.5,
-            price: "100.000",
-            guestsLimit: 4,
-            description: "Casa de campo con piscina y jacuzzi",
-            rules: "No se permite fumar",
-            images: [
-                {
-                    id: 1,
-                    image: "https://img.freepik.com/free-photo/yellow-sofa-wooden-table-living-room-interior-with-plant_41470-3559.jpg?w=2000",
-                    roomId: 3
-                },
-                {
-                    id: 2,
-                    image: "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?cs=srgb&dl=pexels-vecislavas-popa-1571460.jpg&fm=jpg",
-                    roomId: 3
-                },
-                {
-                    id: 3,
-                    image: "https://media.gettyimages.com/photos/bohemian-living-room-interior-3d-render-picture-id1182454657?s=612x612",
-                    roomId: 3
-                }
-            ]
-        },
-
-    ]
-
     useEffect(() => {
         fetch("http://localhost:5000/get-all-rooms")
             .then((resp) => resp.json())
@@ -151,12 +60,12 @@ export function Home({ setUserOn }: any) {
     }
 
     const prevSlide = () => {
-        setCurrent(current === 0 ? roomss.length - 1 : current - 1);
+        setCurrent(current === 0 ? rooms.length - 1 : current - 1);
     };
 
 
     const nextSlide = () => {
-        setCurrent(current === roomss.length - 1 ? 0 : current + 1);
+        setCurrent(current === rooms.length - 1 ? 0 : current + 1);
     };
 
     return (
